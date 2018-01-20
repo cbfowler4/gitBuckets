6f10573a2292f692f60d04ade37592e0449888dd
Bryan <cbfowler4@gmail.com>
2018-01-20 18:32:05 -0500

    data parsed draft 1

--- app.js
+++ app.js
@@ -2,21 +2,10 @@ import seasonData from './data/data';
 import Nodes from './components/nodes';
 import Slider from './components/slider';
 import Store from './store/store';
+import data from './data/team_data.json';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  $.ajax({
-    url: '/team',
-    method: 'get'
-  });
-
-  // const url = `https://stats.nba.com/stats/teamgamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=1610612737&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision=`
-  // console.log(url);
-  //
-  // $.ajax({
-  //   url: url,
-  //   method: 'get'
-  // });
 
   const width = 700;
   const height = 600;
@@ -25,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   .attr("width", width)
   .attr("height", height);
 
-  Store.seasonData = seasonData;
+  Store.seasonData = data;
   Store.nodes = new Nodes(svg, width, height);
 
   const slider = new Slider();
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const nba = require('nba');
-const request = require('postman-request');
+const request = require('request');
+const fs = require('fs');
+
 
 const TEAMS = [
  1610612738,
@@ -40,27 +42,58 @@ const app = express();
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-// app.get('/data', (req, res) => {
-  // TEAMS.forEach((teamId) => {
-  //   nba.stats.teamInfoCommon({LeagueID: "00", Season: req.query.seasonYear, SeasonType: "Regular Season", TeamID: teamId})
-  //   .then((res, err) => console.log(res.teamInfoCommon));
-  //   });
-  // });
+
 app.get('/team', (req, res) => {
-  const url = `https://stats.nba.com/stats/teamgamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=1610612737&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision=`;
-  const url2 = `https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=`;
-  request({
-    uri: url2,
-    method: 'get',
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-      'Accept': 'application/json',
-      'Connection': 'keep-alive'}
-    }, (err, res, body) => {
-    console.log(body);
-  });
+  // const url = `https://stats.nba.com/stats/teamgamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=1610612737&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision=`;
+  getTeamInfo(1998, 2001);
 });
-//https://stats.nba.com/stats/teamgamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=1610612737&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision=
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+
+
+const formatSeason = (startYear) => {
+  const endYear = startYear+1;
+  return `${startYear}-${String(endYear).slice(2)}`;
+};
+
+const getTeamInfo = (startYear, endYear) => {
+  const finalObj = {};
+
+  const makeRequest = (year) => {
+    if (year > endYear) {
+      fs.writeFile('./data/team_data.json', JSON.stringify(finalObj, null, 3));
+      return;
+    }
+    let season = formatSeason(year);
+    console.log(season);
+    const url = `https://stats.nba.com/stats/leaguedashteamstats?Conference=&DateFrom=&DateTo=&Division=&GameScope=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=${season}&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=`;
+    request({
+      uri: url,
+      method: 'get',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+        'Accept': 'application/json',
+        'Connection': 'keep-alive'}
+      }, (err, res, body) => {
+        finalObj[year] = parseTeamResponse(JSON.parse(body));
+        makeRequest(year+1);
+      });
+  };
+
+  makeRequest(startYear);
+};
+
+const parseTeamResponse = (body) => {
+  const parsedResponse = {};
+  console.log(body["resultSets"][0]);
+  body.resultSets[0].rowSet.forEach((team) => {
+    parsedResponse[team[0]] = {
+      w: team[3],
+      l: team[4]
+    };
+  });
+  return parsedResponse;
+};
